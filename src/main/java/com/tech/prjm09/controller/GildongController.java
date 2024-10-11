dba7b37d2919a698fe4276b8019cc038c4093c4e
tbasic5 <tbasic@naver.com>
2024-10-11 17:33:03 +0900

    ddd

--- src/main/java/com/tech/prjm09/controller/GildongController.java
+++ src/main/java/com/tech/prjm09/controller/GildongController.java
@@ -17,8 +17,8 @@ public class GildongController {
 	public void bbb() {
 		System.out.println("bb");
 	}
-	public void ccc() {
-		System.out.println("cc:");
+	public void ddd() {
+		System.out.println("dd:");
 	}
 
 }
