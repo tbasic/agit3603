c0d4506ab55c79c1fa93fbae2127daa9f81924cf
tbasic5 <tbasic@naver.com>
2024-10-11 11:28:00 +0900

    gildongview2

--- src/main/java/com/tech/prjm09/controller/GildongController.java
+++ src/main/java/com/tech/prjm09/controller/GildongController.java
@@ -1,5 +1,14 @@
 package com.tech.prjm09.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GildongController {
+	
+	@RequestMapping("/gildongview")
+	public String gildongview() {
+		return "gildongview";
+	}
 
 }
